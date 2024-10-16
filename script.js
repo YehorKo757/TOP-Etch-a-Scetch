605943e5d4771b3787cab9aba48f344b6da9760c
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-10-16 13:55:06 +0100

    Add UI and functionality to resize grid
    Resizing grid in range ov size values from 0 to 100.
    All values below are set to 0 and all values above are set to 100.

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
 const body = document.querySelector("body");
+
+const containerButton = document.createElement("div");
+containerButton.classList.add("container-button");
+body.appendChild(containerButton);
+
+const button = document.createElement("button");
+button.classList.add("set-size");
+button.textContent = "Resize";
+containerButton.appendChild(button);
+
 const container = document.createElement("div");
 container.classList.add("container");
 body.appendChild(container);
+
+let row = undefined;
 let item = undefined;
-for (let i=0; i<=15; i++){
-    item = document.createElement("div");
-    item.setAttribute("id", "item");
-    container.appendChild(item);
+let size = undefined;
+
+function makeGrid(size = 16) {
+
+    for (let i=0; i<=size-1; i++){
+        row = document.createElement("div");
+        row.classList.add("row");
+    
+        for (let j=0; j<=size-1; j++){
+            item = document.createElement("div");
+            item.setAttribute("id", "item");
+            row.appendChild(item);
+        }
+        container.appendChild(row);
+    }
 }
+
+makeGrid(16);
+
+function clearBody() {
+    while (container.firstChild) {
+        container.firstChild.remove();
+    }
+}
+
+console.log(container.childNodes);
+
+button.addEventListener("click", () => {
+    clearBody();
+    sizeInp = prompt("Please enter required size.\nMaximum valid size is 100.\nAll inputs above would be set to maximum valid value.", "16");
+    sizeInp = parseInt(sizeInp);
+    
+    if (sizeInp > 100) {
+        sizeInp = 100;
+        alert("Size was set to max valid: 100");
+    } else if (sizeInp < 0) {
+        sizeInp = 0;
+        alert("Size was set to min valid: 0");
+    }
+
+    makeGrid(sizeInp);
+
+})
